cbf17ee1f29e71dc9d25477a0f777cbc001355cc
Enzo Seschi <enzosseschi@hotmail.com>
2024-11-13 20:20:33 +0000

    Teste inicio

---
+++ app.js
@@ -0,0 +1,6 @@
+import { prateleira } from "./prateleira.mjs"
+
+/*
+for (let i = 0; i <= 8; i++) {
+    console.log(`Id do produto: ${prateleira["produtos"][i]["id"]} / Nome do produto: ${prateleira["produtos"][i]["atributos"]["nome"]} / Preço: ${prateleira["produtos"][i]["atributos"]["price"]}`)
+}*/
---
+++ prateleira.mjs
@@ -0,0 +1,69 @@
+export const prateleira = 
+{
+    "produtos": 
+    [
+        {
+            "id": 1,
+            "atributos": {
+                "nome": "Fruta1",
+                "price": 0.99
+            }
+        },
+        {
+            "id": 2,
+            "atributos": {
+                "nome": "Fruta2",
+                "price": 1.99
+            }
+        },
+        {
+            "id": 3,
+            "atributos": {
+                "nome": "Fruta3",
+                "price": 2.99
+            }
+        },
+        {
+            "id": 4,
+            "atributos": {
+                "nome": "Carbo1",
+                "price": 3.99
+            }
+        },
+        {
+            "id": 5,
+            "atributos": {
+                "nome": "Carbo2",
+                "price": 4.99
+            }
+        },
+        {
+            "id": 6,
+            "atributos": {
+                "nome": "Carbo3",
+                "price": 2.99
+            }
+        },
+        {
+            "id": 7,
+            "atributos": {
+                "nome": "Prot1",
+                "price": 21.99
+            }
+        },
+        {
+            "id": 8,
+            "atributos": {
+                "nome": "Prot2",
+                "price": 39.99
+            }
+        },
+        {
+            "id": 9,
+            "atributos": {
+                "nome": "Prot3",
+                "price": 51.99
+            }
+        }
+    ]
+}
