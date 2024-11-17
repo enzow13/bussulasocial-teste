aed57c50f1e231f570bbbe47db591512a1cd5d12
Enzo Seschi <enzosseschi@hotmail.com>
2024-11-17 02:55:45 +0000

    Semifinal

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-import { prateleira } from "./prateleira.mjs"
+import { prateleira } from "./prateleira.js"
 
 /*
 for (let i = 0; i <= 8; i++) {
     console.log(`Id do produto: ${prateleira["produtos"][i]["id"]} / Nome do produto: ${prateleira["produtos"][i]["atributos"]["nome"]} / Preço: ${prateleira["produtos"][i]["atributos"]["price"]}`)
 }*/
+
+let carrinho = 0;
+
+function buyItem(num) {
+    let itemPrice = prateleira["produtos"][num]["atributos"]["price"]
+    carrinho += itemPrice
+    document.getElementById("p-totalShopping").innerHTML = `R$ ${carrinho.toFixed(2)}`
+}
+
+window.buyItem = buyItem
--- prateleira.js
+++ prateleira.js
@@ -5,63 +5,63 @@ export const prateleira =
         {
             "id": 1,
             "atributos": {
-                "nome": "Fruta1",
+                "nome": "Alimento1",
                 "price": 0.99
             }
         },
         {
             "id": 2,
             "atributos": {
-                "nome": "Fruta2",
+                "nome": "Alimento2",
                 "price": 1.99
             }
         },
         {
             "id": 3,
             "atributos": {
-                "nome": "Fruta3",
+                "nome": "Alimento3",
                 "price": 2.99
             }
         },
         {
             "id": 4,
             "atributos": {
-                "nome": "Carbo1",
+                "nome": "Alimento4",
                 "price": 3.99
             }
         },
         {
             "id": 5,
             "atributos": {
-                "nome": "Carbo2",
+                "nome": "Alimento5",
                 "price": 4.99
             }
         },
         {
             "id": 6,
             "atributos": {
-                "nome": "Carbo3",
+                "nome": "Alimento6",
                 "price": 2.99
             }
         },
         {
             "id": 7,
             "atributos": {
-                "nome": "Prot1",
+                "nome": "Alimento7",
                 "price": 21.99
             }
         },
         {
             "id": 8,
             "atributos": {
-                "nome": "Prot2",
+                "nome": "Alimento8",
                 "price": 39.99
             }
         },
         {
             "id": 9,
             "atributos": {
-                "nome": "Prot3",
+                "nome": "Alimento9",
                 "price": 51.99
             }
         }
