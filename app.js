eb097d4c17cfc616c7529ec545e0481fb1f204f6
Enzo Seschi <enzosseschi@hotmail.com>
2024-11-17 04:01:20 +0000

    Finalizando...

--- app.js
+++ app.js
@@ -13,4 +13,20 @@ function buyItem(num) {
     document.getElementById("p-totalShopping").innerHTML = `R$ ${carrinho.toFixed(2)}`
 }
 
-window.buyItem = buyItem
+/* Animação goto Pagamento */
+
+function goToPayment() {
+    let article = document.getElementById("article-");
+
+    article.style.transition = "0.8s ease-in";
+    article.style.transform = "translateX(40px)";
+    article.style.opacity = "0";
+    setTimeout(() => {
+        article.style.display = "none";
+    }, 800);
+}
+
+
+/* Subindo módulos */
+window.buyItem = buyItem;
+window.goToPayment = goToPayment;
